04ae4b58571c8e343c24ae62509fa34e1e3ab462
Your Name <you@example.com>
2024-11-11 13:31:43 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  declarations: [AppComponent, TodoListComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, 
+    FormsModule, NgbModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
